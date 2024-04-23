d6829d3f7a64bb208e87fecd35e8cfceae58b13f
Karun Shaji <Karunshaji10@gmail.com>
2024-04-23 17:08:54 +0530

    first commit

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./authSlice";
+import authReducer,{setUserFromLocalStorage} from "./authSlice";
 
 var store = configureStore({
   reducer: {
     auth: authReducer,
   },
 });
+
+store.dispatch(setUserFromLocalStorage());
 export default store;
